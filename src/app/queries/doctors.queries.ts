5c59e36182d1d8628c9339828c2423f22986837b
tluwaniMS <tluwani2020@gmail.com>
2021-12-24 14:18:37 +0200

    adding-doctors-query-properties

--- src/app/queries/doctors.queries.ts
+++ src/app/queries/doctors.queries.ts
@@ -1,5 +1,33 @@
 import { gql } from 'apollo-angular';
 
-export const getDoctorsQuery = gql``;
+export const getDoctorsQuery = gql`
+  query getAllDoctors {
+    id
+    firstName
+    lastName
+    gender
+    email
+    hospital
+    info {
+      specialty
+      hospital
+    }
+  }
+`;
 
-export const getDoctorByIdQuery = gql``;
+export const getDoctorByIdQuery = gql`
+  query GetDoctorById($doctorsId: String!) {
+    getDoctorById(doctorsId: $doctorsId) {
+      id
+      firstName
+      lastName
+      gender
+      email
+      hospital
+      info {
+        specialty
+        hospital
+      }
+    }
+  }
+`;
