a335c00c5c334471c08bda0b23750bdc4397eb93
tluwaniMS <tluwani2020@gmail.com>
2021-12-25 07:49:04 +0200

    adding-doctors-service-functions

--- src/app/services/doctors.endpoint-service.ts
+++ src/app/services/doctors.endpoint-service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import { Observable } from 'rxjs';
 
 @Injectable()
-export class DoctorsEndPointService {}
+export class DoctorsEndPointService {
+  getAllDoctors(): Observable<any> {}
+
+  getDoctorById(doctorsId: number): Observable<any> {}
+
+  deleteDoctorById(doctorsId: number): Observable<any> {}
+}
