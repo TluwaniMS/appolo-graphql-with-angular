87face82756e4b54ef5b36ca3e68352e8beb0b88
tluwaniMS <tluwani2020@gmail.com>
2021-12-25 07:21:41 +0200

    configuring-end-point-services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { DoctorsEndPointService } from 'src/app/services/doctors.endpoint-service';
+import { HospitalsEndPointService } from 'src/app/services/hospitals.endpoint.service';
+import { MunicipalitiesEndPointService } from 'src/app/services/municipalities.endpoint.service';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, GraphQLModule, HttpClientModule],
+  providers: [
+    DoctorsEndPointService,
+    HospitalsEndPointService,
+    MunicipalitiesEndPointService,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    GraphQLModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
