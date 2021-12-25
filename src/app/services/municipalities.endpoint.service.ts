60f83b087a7adb6f2a21b0f2de5f7657cd41be0a
tluwaniMS <tluwani2020@gmail.com>
2021-12-25 08:07:41 +0200

    adding-municipality-service-functions

--- src/app/services/municipalities.endpoint.service.ts
+++ src/app/services/municipalities.endpoint.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import { Observable } from 'rxjs';
+import {
+  getMunicipalitiesQuery,
+  getMunicipalityByIdQuery,
+} from 'src/app/queries/municipalities.queries';
+import { deleteMunicipality } from 'src/app/mutations/municipalities.mutations';
 
 @Injectable()
-export class MunicipalitiesEndPointService {}
+export class MunicipalitiesEndPointService {
+  getAllMunicipalities(): Observable<any> {}
+
+  getMunicipalityById(municipalityKey: string): Observable<any> {}
+
+  deleteMunicipalityById(municipalityKey: string): Observable<any> {}
+}
