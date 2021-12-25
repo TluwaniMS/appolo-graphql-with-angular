e8d4e6ea2ecebe9817f8a002c94699f37211a763
tluwaniMS <tluwani2020@gmail.com>
2021-12-25 07:36:40 +0200

    adding-hospital-mutation-properties

--- src/app/mutations/hospitals.mutations.ts
+++ src/app/mutations/hospitals.mutations.ts
@@ -1,1 +1,7 @@
 import { gql } from 'apollo-angular';
+
+export const deleteHospital = gql`
+  mutation DeleteHospitalById($hospitalKey: String!) {
+    deleteHospitalById(hospitalKey: $hospitalKey)
+  }
+`;
