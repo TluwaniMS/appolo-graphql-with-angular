744001cef50ce7a5e97729afb03fa6199a9ef618
tluwaniMS <tluwani2020@gmail.com>
2021-12-25 08:02:44 +0200

    adding-doctors-and-hospital-query-types

--- src/app/services/doctors.endpoint-service.ts
+++ src/app/services/doctors.endpoint-service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import { Observable } from 'rxjs';
+import {
+  getDoctorByIdQuery,
+  getDoctorsQuery,
+} from 'src/app/queries/doctors.queries';
+import { deleteDoctor } from 'src/app/mutations/doctors.mutations';
 
 @Injectable()
 export class DoctorsEndPointService {
--- src/app/services/hospitals.endpoint.service.ts
+++ src/app/services/hospitals.endpoint.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import { Observable } from 'rxjs';
+import {
+  getHospitalByIdQuery,
+  getHospitalsQuery,
+} from 'src/app/queries/hospitals.queries';
+import { deleteHospital } from 'src/app/mutations/hospitals.mutations';
 
 @Injectable()
 export class HospitalsEndPointService {
